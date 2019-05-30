099d9e2150fe4dec39285b8386eb5f3dba340678
Phuc Tran <phuctran00715@outlook.com.vn>
2019-05-30 21:35:54 +0700

    remove not used

--- ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp12")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp12")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f0fab3d7-8086-44ad-91b6-67838e429b90")]
+[assembly: Guid("e05a8a32-f26b-4219-89e2-0dc3cb56070c")]
 
 // Version information for an assembly consists of the following four values:
 //
